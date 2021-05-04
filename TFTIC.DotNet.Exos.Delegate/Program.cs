d24828cb4b1d2277052458cb4d2d0a1578909d15
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-04 14:27:39 +0200

    Update Exo - Delegate (++)

--- TFTIC.DotNet.Exos.Delegate/Program.cs
+++ TFTIC.DotNet.Exos.Delegate/Program.cs
@@ -2,29 +2,14 @@
 
 namespace TFTIC.DotNet.Exos.Delegate
 {
-    public delegate void MouvementRobot();
     class Program
     {
         static void Main(string[] args)
         {
             Robot r = new Robot();
-
-            MouvementRobot mr;
-            mr = r.Avancer;
-            mr += r.TournerGauche;
-            mr += r.Avancer;
-            mr += r.TournerDroite;
-            mr += r.Avancer;
-            mr += r.TournerGauche;
-            mr += r.Avancer;
-            mr += r.TournerDroite;
-            mr += r.Avancer;
-            mr += r.TournerDroite;
-            mr += r.Avancer;
-            mr += r.Avancer;
-
-            mr();
-
+            r.AddMouvement(Robot.Movement.Avancer);
+            r.AddMouvement(Robot.Movement.Gauche, Robot.Movement.Avancer, Robot.Movement.Droite);
+            r.StartMouvement();
         }
     }
 }
--- TFTIC.DotNet.Exos.Delegate/Robot.cs
+++ TFTIC.DotNet.Exos.Delegate/Robot.cs
@@ -4,14 +4,17 @@ using System.Text;
 
 namespace TFTIC.DotNet.Exos.Delegate
 {
+    public delegate void MouvementRobot();
     public enum Direction { Haut, Droite, Bas, Gauche }
     public class Robot
     {
-        public int Position_X;
-        public int Position_Y;
+        public enum Movement { Gauche, Avancer, Droite }
+        private int Position_X;
+        private int Position_Y;
         private int _direction_angle;
+        private MouvementRobot _mr;
 
-        public Direction Direction
+        private Direction Direction
         {
             get
             {
@@ -20,15 +23,15 @@ namespace TFTIC.DotNet.Exos.Delegate
                 {
                     case -270:
                     case 90:
-                        d= Direction.Droite;
+                        d = Direction.Droite;
                         break;
                     case -180:
-                    case 180: 
-                        d= Direction.Bas;
+                    case 180:
+                        d = Direction.Bas;
                         break;
                     case -90:
-                    case 270: 
-                        d= Direction.Gauche;
+                    case 270:
+                        d = Direction.Gauche;
                         break;
                 }
                 return d;
@@ -39,9 +42,10 @@ namespace TFTIC.DotNet.Exos.Delegate
         {
             Position_X = Position_Y = 0;
             _direction_angle = 90;
+            _mr = null;
         }
 
-        public void Avancer()
+        private void Avancer()
         {
             switch (this.Direction)
             {
@@ -60,19 +64,61 @@ namespace TFTIC.DotNet.Exos.Delegate
             }
             Console.WriteLine(this);
         }
-        public void TournerGauche()
+        private void TournerGauche()
         {
             _direction_angle -= 90;
             Console.WriteLine(this);
         }
-        public void TournerDroite()
+        private void TournerDroite()
         {
             _direction_angle += 90;
             Console.WriteLine(this);
         }
 
-        //virtual string ToString() => Return NomNameSpace.NomObject //TFTIC.DotNet.Exos.Delegate.Robot
+        public void AddMouvement(Movement action)
+        {
+            switch (action)
+            {
+                case Movement.Gauche:
+                    _mr += TournerGauche;
+                    break;
+                case Movement.Avancer:
+                    _mr += Avancer;
+                    break;
+                case Movement.Droite:
+                    _mr += TournerDroite;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void AddMouvement(params Movement[] actions)
+        {
+            foreach (Movement action in actions)
+            {
+                switch (action)
+                {
+                    case Movement.Gauche:
+                        _mr += TournerGauche;
+                        break;
+                    case Movement.Avancer:
+                        _mr += Avancer;
+                        break;
+                    case Movement.Droite:
+                        _mr += TournerDroite;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
 
+        public void StartMouvement()
+        {
+            _mr();
+        }
+
+        //virtual string ToString() => Return NomNameSpace.NomObject //TFTIC.DotNet.Exos.Delegate.Robot
         public override string ToString()
         {
             return $"Pos X: {this.Position_X} - Y : {this.Position_Y} | Direction : {this.Direction}";
